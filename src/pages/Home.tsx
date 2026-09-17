eeed8651970cb51fae4a550b94dea08ef94a2963
Ben Pepers <pepersben@gmail.com>
2026-09-17 16:46:04 +0100

    add spotify playlist

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -59,6 +59,15 @@ export default function Home() {
       </div>
       <p className="text-base sm:text-lg">Thursday 29th July 2027</p>
       <p className="text-base sm:text-lg">Rise Hall, Beverley</p>
+      <iframe
+        data-testid="embed-iframe"
+        src="https://open.spotify.com/embed/playlist/7nmTrtXJw6xFgwlRnQ3kdz?utm_source=generator&theme=0&si=d4d1dd68f0194f93"
+        width="100%"
+        height="152"
+        frameBorder="0"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
     </div>
   );
 }
