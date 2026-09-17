169eba782a86fb4bd364ee47e92452b34ab696c1
Ben Pepers <pepersben@gmail.com>
2026-09-17 22:59:51 +0100

    add faq tests

--- src/pages/__tests__/FAQ.test.tsx
+++ src/pages/__tests__/FAQ.test.tsx
@@ -5,7 +5,9 @@ describe("FAQ", () => {
   test("renders each question as an accordion item", () => {
     render(<FAQ />);
 
-    expect(screen.getByRole("heading", { name: "Frequently Asked Questions" })).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { name: "Frequently Asked Questions" }),
+    ).toBeInTheDocument();
     expect(screen.getAllByRole("group")).toHaveLength(12);
     expect(screen.getByText("What should I wear?")).toBeInTheDocument();
     expect(screen.getByText("Is Rise Hall accessible?")).toBeInTheDocument();
@@ -15,7 +17,13 @@ describe("FAQ", () => {
     render(<FAQ />);
 
     expect(screen.getByText(/The dress code is formal/)).toBeInTheDocument();
-    expect(screen.getByText(/The music will finish at 11.45pm/)).toBeInTheDocument();
-    expect(screen.getByText(/The bar will accept card and contactless payments only/)).toBeInTheDocument();
+    expect(
+      screen.getByText(/The music will finish at 11.45pm/),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        /The bar will accept card and contactless payments only/,
+      ),
+    ).toBeInTheDocument();
   });
 });
